34846f615c7f422aef58bebd0bc793a44905fa21
juliana-santiago <juju_msantiago@hotmail.com>
2020-07-13 20:25:04 -0300

    Star Wars 2.0

--- starwars-react/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders Test React link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Test React/i);
-  expect(linkElement).toBeInTheDocument();
-});
